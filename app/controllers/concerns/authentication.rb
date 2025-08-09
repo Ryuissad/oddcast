b256ff96c5ec1f36ca82bc36cdfe6288a1aa3e3c
Arkrawit Pichedvanichok <dragonryuplus@gmail.com>
2025-08-09 15:54:56 +0700

    fix

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -2,13 +2,13 @@ module Authentication
   extend ActiveSupport::Concern
 
   included do
-    before_action :require_authentication
+    # before_action :require_authentication
     helper_method :authenticated?
   end
 
   class_methods do
     def allow_unauthenticated_access(**options)
-      skip_before_action :require_authentication, **options
+      # skip_before_action :require_authentication, **options
     end
   end
 
