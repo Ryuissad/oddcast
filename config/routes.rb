32d4e8165688a59c5a3556e17c88fc5374bffc8b
Arkrawit Pichedvanichok <dragonryuplus@gmail.com>
2025-08-09 14:31:21 +0700

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :articles
+
+    root "articles#index"
   end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
