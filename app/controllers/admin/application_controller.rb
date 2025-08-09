93c46a339cad7673ea0a85e7600eb2a4a3e17c07
Arkrawit Pichedvanichok <dragonryuplus@gmail.com>
2025-08-09 16:59:55 +0700

    add enum

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,6 +1,5 @@
 module Admin
   class ApplicationController < ::ApplicationController
-    include Authentication
-      before_action :require_authentication
+    before_action :require_authentication
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
     has_one_attached :cover_image
+
+    enum :status, [ :draft, :wating_for_review, :published ]
 end
